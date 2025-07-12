d1242191c81ab9ef14375222f1d8616b22a12976
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-07-12 09:37:37 +0330

    feat: add UITabs component

---
+++ components/ui/Tabs.vue
@@ -0,0 +1,35 @@
+<template>
+  <UTabs
+    color="primary"
+    variant="link"
+    :content="content"
+    :items="items"
+    :ui="{
+      list: `tabs-list primary-box overflow-hidden border-b-0 justify-center sm:justify-start sm:px-4 items-center py-1.5`,
+      indicator: 'h-1.5 bottom-auto bottom-[-2px] ',
+      trigger: 'font-semibold',
+    }"
+  >
+    <template #content>
+      <slot name="content" />
+    </template>
+  </UTabs>
+</template>
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+  content: {
+    type: Boolean,
+  },
+});
+</script>
+<style>
+@reference "tailwindcss";
+.tabs-list::before {
+  @apply hidden sm:block h-full text-sm pe-4 text-[var(--ui-gray)];
+  content: "مرتب سازی بر اساس:";
+}
+</style>
