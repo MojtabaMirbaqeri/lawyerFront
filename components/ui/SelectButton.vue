4d3b351523b0e7bb57a8ebdd21bc5db02b56df04
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-07-14 09:16:17 +0330

    feat: add SelectButton.vue

--- components/ui/SelectButton.vue
+++ components/ui/SelectButton.vue
@@ -2,31 +2,64 @@
   <div class="flex flex-wrap justify-start items-start gap-x-2 gap-y-3">
     <button
       v-for="item in items"
-      :key="item.value"
-      :class="[baseClass, modelValue === item.value ? activeClass : '']"
-      @click="() => (modelValue = item.value)"
+      :key="item.id"
+      class="flex items-center gap-1"
+      :class="[baseClass, isSelected(item.id) ? activeClass : '']"
+      @click="handleClick(item.id)"
     >
-      {{ item.label }}
+      <UIcon
+        v-if="item.icon"
+        :name="item.icon"
+        :class="['size-4!', item.iconClass]"
+      />
+      {{ item.title }}
     </button>
   </div>
 </template>
-  
-  <script setup>
-const modelValue = defineModel({ type: String });
 
-defineProps({
+<script setup>
+const modelValue = defineModel({ type: [String, Array] });
+
+const props = defineProps({
   items: {
     type: Array,
     required: true,
+    //  { title: string, id: any, icon?: string }
   },
   baseClass: {
     type: String,
     default:
-      "px-4 py-2 rounded-full border border-primary text-sm lg:text-base transition cursor-pointer",
+      "px-4 py-2 rounded-full border border-gray-300 text-sm lg:text-base transition cursor-pointer",
   },
   activeClass: {
     type: String,
-    default: "bg-primary! text-white!",
+    default: "bg-primary! border-transparent! text-white!",
+  },
+  multiple: {
+    type: Boolean,
+    default: false,
   },
 });
+
+const isSelected = (id) => {
+  return props.multiple
+    ? Array.isArray(modelValue.value) && modelValue.value.includes(id)
+    : modelValue.value === id;
+};
+const handleClick = (id) => {
+  if (props.multiple) {
+    const current = Array.isArray(modelValue.value)
+      ? [...modelValue.value]
+      : [];
+    const index = current.indexOf(id);
+    if (index >= 0) {
+      current.splice(index, 1);
+    } else {
+      current.push(id);
+    }
+    modelValue.value = current;
+  } else {
+    modelValue.value = id;
+  }
+};
 </script>
