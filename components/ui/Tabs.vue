2616fbee6e4f1eba99b274436d5317b3627ce342
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-07-14 12:15:44 +0330

    style: remove static label

--- components/ui/Tabs.vue
+++ components/ui/Tabs.vue
@@ -6,7 +6,7 @@
     :content="content"
     :items="items"
     :ui="{
-      list: `tabs-list primary-box overflow-hidden border-b-0 justify-center sm:justify-start sm:px-4 items-center py-1.5`,
+      list: `tabs-list primary-box overflow-hidden border-b-0 justify-center sm:justify-start sm:px-4 items-center py-1.5 md:py-2`,
       indicator: 'h-1.5 bottom-auto bottom-[-2px] ',
       trigger: 'font-semibold cursor-pointer',
     }"
@@ -30,10 +30,3 @@ const selectedTab = defineModel({
   type: String,
 });
 </script>
-<style>
-@reference "tailwindcss";
-.tabs-list::before {
-  @apply hidden sm:block h-full text-sm pe-4 text-[var(--ui-gray)];
-  content: "مرتب سازی بر اساس:";
-}
-</style>
