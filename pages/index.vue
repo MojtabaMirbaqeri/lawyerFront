514588d0898dec97fabaccc0fbcdc9aae096a4d6
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-07-15 11:52:05 +0330

    feat: create filters and sidebar and add them to index

--- pages/index.vue
+++ pages/index.vue
@@ -9,22 +9,96 @@
         },
       ]"
     />
-    <div class="container pt-0">
-      <LandingFilters />
-      <main class="space-y-4">
+    <div class="container pt-0 space-y-4">
+      <div class="space-y-4 xl:space-y-5">
         <div class="space-y-4">
-          <UISecondaryBtn>
-            مشاهده همه <UIcon name="proicons:chevron-left" />
-          </UISecondaryBtn>
-          <UISelectButton v-model="testVal" :items="testItems" />
+          <UIDrawer
+            title="lawyerTypes"
+            description="Categorization of lawyers by type"
+          >
+            <template #button>
+              <UISecondaryBtn>
+                مشاهده همه <UIcon name="proicons:chevron-left" />
+              </UISecondaryBtn>
+            </template>
+            <template #default>
+              <div class="flex flex-col gap-2 text-black">
+                <div
+                  v-for="type in lawyerTypes"
+                  :key="type.id"
+                  class="flex items-center justify-between py-2 px-2 rounded-lg bg-gray-100 border border-gray-200 transition-all duration-200 cursor-pointer"
+                  :class="{
+                    'bg-gray-200!  border-gray-400!':
+                      type.id == selectedLawyerType,
+                  }"
+                  @click="selectedLawyerType = type.id"
+                >
+                  {{ type.title }}
+                  <UIcon name="proicons:chevron-left" class="ms-auto" />
+                </div>
+              </div>
+            </template>
+          </UIDrawer>
+          <UISelectButton
+            v-model="selectedLawyerType"
+            :items="lawyerTypes.slice(0, 6)"
+          />
         </div>
-        <div class="filters-trigger primary-box">
-          <UIcon name="system-uicons:filtering" class="size-6! text-primary" />
-          فیلتر ها
-          <UIcon name="proicons:chevron-left" class="ms-auto" />
-        </div>
-        <UITabs v-model="selectedTab" :content="false" :items="tabItems" />
-      </main>
+        <UIDrawer
+          title="filters"
+          description="filter laywers"
+          class="overflow-y-auto!"
+        >
+          <template #button>
+            <div class="filters-trigger primary-box">
+              <UIcon
+                name="system-uicons:filtering"
+                class="size-6! text-primary"
+              />
+              فیلتر ها
+              <UIcon name="proicons:chevron-left" class="ms-auto" />
+            </div>
+          </template>
+          <template #default><LandingFilters /></template>
+        </UIDrawer>
+      </div>
+      <div class="lg:flex gap-4 xl:gap-5 items-start">
+        <LandingSidebar class="hidden lg:block sticky top-[90px]" />
+        <main class="space-y-4 grow">
+          <UITabs
+            v-model="selectedTab"
+            :content="false"
+            :items="tabItems"
+            class="sort-tabs"
+          />
+          <div class="lawyers-con">
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+            <NuxtLink to="/lawyer/5">
+              <LawyerCard />
+            </NuxtLink>
+          </div>
+        </main>
+      </div>
     </div>
   </main>
 </template>
@@ -46,22 +120,32 @@ const tabItems = ref([
 
 const selectedTab = ref(tabItems.value[0].value);
 
-const testItems = [
-  { label: "همه", value: "all" },
-  { label: "وکیل پایه یک دادگستری", value: "1" },
-  { label: "وکیل پایه دو دادگستری", value: "2" },
-  { label: "کارآموز وکالت", value: "3" },
-  { label: "وکیل تسخیری", value: "4" },
-  { label: "وکیل معاضدتی", value: "5" },
-  { label: "وکیل اتفاقی", value: "6" },
-  { label: "وکیل موضوع ماده 187", value: "7" },
+const lawyerTypes = [
+  { title: "همه", id: "all" },
+  { title: "وکیل پایه یک دادگستری", id: "1" },
+  { title: "وکیل پایه دو دادگستری", id: "2" },
+  { title: "کارآموز وکالت", id: "3" },
+  { title: "وکیل تسخیری", id: "4" },
+  { title: "وکیل معاضدتی", id: "5" },
+  { title: "وکیل اتفاقی", id: "6" },
+  { title: "وکیل موضوع ماده 187", id: "7" },
 ];
 
-const testVal = ref(testItems[0].value);
+const selectedLawyerType = ref(lawyerTypes[0].id);
 </script>
 <style scoped>
 @reference "tailwindcss";
 .filters-trigger {
-  @apply cursor-pointer flex items-center gap-2 font-semibold;
+  @apply cursor-pointer flex items-center gap-2 font-semibold lg:hidden;
+}
+</style>
+<style>
+@reference "tailwindcss";
+.sort-tabs .tabs-list::before {
+  @apply hidden sm:block h-full text-sm pe-4 text-[var(--ui-gray)];
+  content: "مرتب سازی بر اساس:";
+}
+.lawyers-con {
+  @apply flex flex-col gap-3 pb-4 lg:pb-6;
 }
 </style>
