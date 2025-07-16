fb3cf106d157cd25e9c9223c3879424c6a63f86e
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-07-16 10:59:19 +0330

    chore: add UIThing CLI

---
+++ ui-thing.config.ts
@@ -0,0 +1,12 @@
+export default {
+  "nuxtVersion": 3,
+  "tailwindCSSLocation": "assets/css/main.css",
+  "tailwindConfigLocation": "tailwind.config.js",
+  "componentsLocation": "components/Thing",
+  "composablesLocation": "composables",
+  "pluginsLocation": "plugins",
+  "utilsLocation": "utils",
+  "force": true,
+  "useDefaultFilename": true,
+  "packageManager": "npm"
+}
