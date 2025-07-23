6234b91a86348ab4d3b91d39948a6db7917ff88e
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-07-23 10:14:25 +0330

    feat: add authIncluded to useGet composable

--- app.vue
+++ app.vue
@@ -7,17 +7,7 @@
 </template>
 
 <script setup>
-const jwtToken = useCookie("jwtToken");
-jwtToken.value = "3|3QrgsoERXk3sSCY2xDpNqu7819taDstpeKyLEN0N370280be";
 
-const res = await useGet("lawyers", "");
-const data = await res.data;
-
-const lawyers = data.data
-onMounted(async () => {
-  // console.log(useUserAuthStore().userToken);
-
-
-  // console.log(lawyers[0]);
-});
+const { data } = await useGet("lawyers");
+console.log(data);
 </script>
