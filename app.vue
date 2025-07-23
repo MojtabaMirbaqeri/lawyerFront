d0e11466229686fb909c84fbf5b4fb7b43d91a98
MTahaHaj8686 <mohammadtahaworkhaji8686@gmail.com>
2025-07-23 12:24:33 +0330

    feat:connect detail of lawyer api

--- app.vue
+++ app.vue
@@ -10,14 +10,14 @@
 const jwtToken = useCookie("jwtToken");
 jwtToken.value = "3|3QrgsoERXk3sSCY2xDpNqu7819taDstpeKyLEN0N370280be";
 
-const res = await useGet("lawyers", "");
-const data = await res.data;
+// const res = await useGet("lawyers", "");
+// const data = await res.data;
 
-const lawyers = data.data
+// const lawyers = data.data
 onMounted(async () => {
   // console.log(useUserAuthStore().userToken);
 
 
-  // console.log(lawyers[0]);
+  // console.log(lawyers);
 });
 </script>
--- pages/lawyer/[id].vue
+++ pages/lawyer/[id].vue
@@ -15,18 +15,17 @@
     <div class="container gap-4 flex flex-col lg:flex-row">
       <div class="right flex flex-col gap-4">
         <info-lawyer-card
-          fullname="حلیمه صابریان"
-          :active="true"
-          avatar="/images/avatar.png"
-          education="وکیل پایه یک دادگستری"
-          experience="8"
-          rate="4.9"
-          city="بابل"
-          visit="2456"
+          :fullname="lawyer.lawyer_info.name+' '+lawyer.lawyer_info.family"
+          :active="lawyer.is_active"
+          :avatar="lawyer.lawyer_info.profile_image"
+          :education="lawyer.base"
+          :experience="lawyer.years_of_experience"
+          :rate="lawyer.average_rating.split('').splice(0, 3).join('')"
+          :city="lawyer.city"
         />
         <info-lawyer-WhyOnlineVisit />
         <InfoLawyerChooseVisit class="block lg:hidden" />
-        <info-lawyer-tab />
+        <info-lawyer-tab :dis="lawyer.lawyer_info.about" :pos="[+lawyer.latitude,+lawyer.longitude]"/>
         <info-lawyer-comment />
       </div>
       <div class="left w-[150%] hidden lg:block">
@@ -37,6 +36,13 @@
 </template>
 
 <script setup>
+const res = await useGet(`lawyers/${useRoute().params.id}`,"")
+const data = await res.data
+const lawyer = ref(data.data)
+
+onMounted(() => {
+  console.log(lawyer.value.id);
+})
 </script>
 
 <style scoped></style>
