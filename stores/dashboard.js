eab878b51ed2a6ef3e5be782cdd219e7d4b894d2
MTahaHaj8686 <mohammadtahaworkhaji8686@gmail.com>
2025-08-09 11:18:15 +0330

    fix:fix dashboard store

--- stores/dashboard.js
+++ stores/dashboard.js
@@ -5,7 +5,7 @@ export const useDashboardStore = defineStore("dashboard", () => {
     { title: "داشبورد", url: "/dashboard", icon: "lucide:home" },
     { title: "لیست وکلا", url: "/dashboard/admin/lawyerlist", icon: "lucide:inbox" },
     { title: "انتظار تایید", url: "/dashboard/admin/registerlawyer", icon: "material-symbols:fingerprint" },
-    { title: "لیست کاربران", url: "#", icon: "lucide:calendar" },
+    { title: "لیست کاربران", url: "/dashboard/admin/users", icon: "lucide:calendar" },
     { title: "صورت حساب", url: "#", icon: "lucide:search" },
     { title: "تنظیمات", url: "#", icon: "lucide:settings" },
   ];
