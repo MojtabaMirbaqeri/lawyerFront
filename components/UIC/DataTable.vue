9f95c2bf27cdbf5a41b33d7c6f6bebc2dfdb5a81
MTahaHaj8686 <mohammadtahaworkhaji8686@gmail.com>
2025-08-09 11:26:52 +0330

    feat:create user table

---
+++ components/UIC/DataTable.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { h, resolveComponent } from "vue";
+import type { TableColumn } from "@nuxt/ui";
+
+const UBadge = resolveComponent("UBadge");
+const props = defineProps(["data", "columns"]);
+
+type Payment = {
+  id: string;
+  date: string;
+  status: "paid" | "failed" | "refunded";
+  email: string;
+  amount: number;
+};
+</script>
+
+<template>
+  <UTable
+    :data="data"
+    :ui="{
+      root: 'rounded-[7px]',
+      thead: 'bg-primary',
+      th: 'text-white text-center!',
+      td: 'text-center',
+    }"
+    :columns="columns"
+    class="flex-1"
+  />
+</template>
