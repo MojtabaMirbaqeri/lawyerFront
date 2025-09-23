adf66020b76586656ed7daed49dc48f4992cec39
MojtabaMirbaqeri <159177290+MojtabaMirbaqeri@users.noreply.github.com>
2025-09-23 14:04:12 +0330

    feat: edit location in LawyerProfile

--- components/Lawyer/Profile.vue
+++ components/Lawyer/Profile.vue
@@ -6,8 +6,7 @@
         class="size-full"
         :show="show"
         position="bottom-right"
-        :ui="{ base: 'bg-blue-400 size-3 right-[10%]' }"
-      >
+        :ui="{ base: 'bg-blue-400 size-3 right-[10%]' }">
         <UAvatar
           class="size-full"
           :src="
@@ -19,33 +18,34 @@
           "
           :ui="{
             image: 'object-[50%_0%]',
-          }"
-        />
+          }" />
       </UChip>
     </div>
     <div class="person-detail w-full">
       <div
         class="fullname font-semibold flex justify-between lg:justify-start lg:gap-2"
-        :class="[ui?.name]"
-      >
-        {{
-          `${information.lawyer_info.name} ${information.lawyer_info.family}`
-        }}
+        :class="[ui?.name]">
+        {{ `${information.lawyer_info.name} ${information.lawyer_info.family}` }}
         <div class="flex items-center gap-1">
           <slot name="badges" />
           <UICBadge
             :value="information.is_active ? 'فعال' : 'غیرفعال'"
             icon="mynaui:clock-square-solid"
-            :variant="information.is_active ? 'blue' : 'gray'"
-          />
+            :variant="information.is_active ? 'blue' : 'gray'" />
         </div>
       </div>
       <div class="education">{{ information.base }}</div>
       <div>
         {{
           showExperience
-            ? `تجربه: ${information?.years_of_experience} سال`
-            : `محل کار: ${information.province}، ${information.city}`
+            ? `تجربه: ${information?.years_of_experience ?? "ثبت نشده"} سال`
+            : information?.province && information?.city
+            ? `محل کار: ${information.province}، ${information.city}`
+            : information?.province
+            ? `محل کار: ${information.province}`
+            : information?.city
+            ? `محل کار: ${information.city}`
+            : "محل کار: ثبت نشده"
         }}
       </div>
     </div>
@@ -56,5 +56,4 @@
 defineProps(["information", "show", "ui", "showExperience"]);
 </script>
 
-<style>
-</style>
+<style></style>
