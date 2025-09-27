7211b878ca89574ee5d4a3aec959aadc6de60918
MTahaHaj8686 <mohammadtahaworkhaji8686@gmail.com>
2025-09-27 09:42:53 +0330

    style:edit this file

--- app.config.ts
+++ app.config.ts
@@ -9,8 +9,8 @@ export default defineAppConfig({
     },
     tabs:{
       slots:{
-              root: 'primary-box sm:px-4 py-0',
-      list: `tabs-list overflow-hidden border-b-0 justify-center py-1.5 sm:justify-start  items-center `,
+              root: 'sm:px-4 py-0',
+      list: `tabs-list overflow-hidden border-b-0 justify-center py-1.5 border-none  sm:justify-start  items-center `,
       indicator: 'h-1.5! bottom-auto! bottom-[-2px]!',
       trigger: 'font-semibold cursor-pointer text-[13px] lg:text-sm',
       content: 'py-4',
