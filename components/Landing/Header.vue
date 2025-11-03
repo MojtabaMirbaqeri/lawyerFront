0693ec5d4dea3df85bb1209c88fd660e9e805061
MTahaHaj8686 <mohammadtahaworkhaji8686@gmail.com>
2025-11-03 10:49:53 +0330

    style:change style of header and change favicon

--- components/Landing/Header.vue
+++ components/Landing/Header.vue
@@ -1,116 +1,121 @@
 <template>
   <header>
     <div class="container">
-      <h1 class="header-title">
-        دریافت خدمات مشاوره از
-        <ClientOnly>{{ useGlobalStore()?.lawyersCount?.toLocaleString() }}</ClientOnly>
-        وکیل متخصص <br />
-        در سراسر کشور
-      </h1>
-      <div class="w-full">
-        <div class="relative z-50" ref="searchWrapper">
-          <UInput
-            v-model="lawyerNameFilter"
-            class="w-full relative z-10"
-            name="laweyerNameFilter"
-            placeholder="جست و جوی وکلا"
-            :ui="{
-              base: 'w-full rounded-full border border-gray-200 py-3 md:text-base  ring-0 focus-visible:ring-0 ps-[134px] sm:ps-[178px]',
-              trailing: 'pe-0 m-1',
-              leading: 'ps-0 m-1.5',
-            }"
-            @keydown.enter="search"
-            @focus="handleFocus">
-            <template #trailing>
-              <div class="search-btn" @click="search">
-                <UIcon name="hugeicons:search-02" class="size-5! text-white" />
+      <div class="flex items-center justify-between flex-col gap-4">
+        <h1 class="header-title">
+          دریافت خدمات مشاوره از
+          <ClientOnly>{{ useGlobalStore()?.lawyersCount?.toLocaleString() }}</ClientOnly>
+          وکیل متخصص <br />
+          در سراسر کشور
+        </h1>
+        <div class="w-full">
+          <div class="relative z-50" ref="searchWrapper">
+            <UInput
+              v-model="lawyerNameFilter"
+              class="w-full relative z-10"
+              name="laweyerNameFilter"
+              placeholder="جست و جوی وکلا"
+              :ui="{
+                base: 'w-full rounded-full border bg-[#E1E1E3] border-[#00000047] py-3 md:text-base  ring-0 focus-visible:ring-0 ps-[134px] sm:ps-[178px]',
+                trailing: 'pe-0 m-1',
+                leading: 'ps-0 m-1.5',
+              }"
+              @keydown.enter="search"
+              @focus="handleFocus">
+              <template #trailing>
+                <div class="search-btn" @click="search">
+                  <UIcon name="hugeicons:search-02" class="size-6! text-[#919191]" />
+                </div>
+              </template>
+              <template #leading>
+                <USelectMenu
+                  v-model="filtersStore.selectedFilters.province"
+                  class="h-full"
+                  :ui="{
+                    base: 'font-semibold bg-[#E1E1E3] ring-[#00000047]! text-xs sm:text-sm rounded-full w-[120px] sm:w-[160px]',
+                    itemLabel: 'text-[13px]! sm:text-sm!',
+                    leadingIcon: 'text-[#919191] size-5!',
+                    trailingIcon: 'size-4! text-[#919191]',
+                  }"
+                  :items="provinces"
+                  value-key="id"
+                  label-key="name"
+                  icon="i-ph-map-pin-duotone"
+                  :search-input="{
+                    placeholder: 'جستجو...',
+                  }" />
+              </template>
+            </UInput>
+  
+            <!-- Suggest Box -->
+            <Transition name="fade">
+              <div v-if="showSuggestBox" class="suggest-box text-secondary">
+                <!-- Loader -->
+                <div v-if="loading" class="space-y-2">
+                  <USkeleton v-for="i in 3" :key="i" class="h-20 w-full rounded-xl" />
+                </div>
+  
+                <!-- Lawyers list -->
+                <div
+                  v-else-if="lawyers?.length"
+                  class="max-h-72 overflow-y-auto flex flex-col gap-2">
+                  <NuxtLink
+                    v-for="lawyer in lawyers"
+                    :key="lawyer.id"
+                    :to="`/lawyer/${lawyer.id}`">
+                    <LawyerProfile
+                      class="border border-gray-200 p-3 rounded-xl"
+                      :information="{
+                        ...lawyer,
+                        lawyer_info: {
+                          name: lawyer.name,
+                          family: lawyer.family,
+                          profile_image: lawyer.profile_image,
+                        },
+                      }"
+                      :show="true"
+                      :show-experience="false"
+                      :ui="{
+                        base: 'items-start! w-full!',
+                        avatar: 'size-14! lg:size-16!',
+                        name: 'justify-between!',
+                      }">
+                      <template #badges>
+                        <UICBadge
+                          variant="yellow"
+                          icon-size="size-4! lg:size-5!"
+                          :value="Number(lawyer.average_rating).toFixed(1)"
+                          icon="ic:round-star" />
+                      </template>
+                    </LawyerProfile>
+                  </NuxtLink>
+                </div>
+                <div
+                  v-else
+                  class="flex items-center gap-2 p-4 justify-center border border-gray-200 rounded-xl bg-gray-50">
+                  <UIcon
+                    name="hugeicons:information-diamond"
+                    class="align-middle size-6!" />
+                  <h1 class="font-semibold text-lg">وکیلی یافت نشد</h1>
+                </div>
               </div>
-            </template>
-            <template #leading>
-              <USelectMenu
-                v-model="filtersStore.selectedFilters.province"
-                class="h-full"
-                :ui="{
-                  base: 'font-semibold text-xs sm:text-sm rounded-full w-[120px] sm:w-[160px]',
-                  itemLabel: 'text-[13px]! sm:text-sm!',
-                  leadingIcon: 'text-primary size-5!',
-                  trailingIcon: 'size-4!',
-                }"
-                :items="provinces"
-                value-key="id"
-                label-key="name"
-                icon="i-ph-map-pin-duotone"
-                :search-input="{
-                  placeholder: 'جستجو...',
-                }" />
-            </template>
-          </UInput>
-
-          <!-- Suggest Box -->
-          <Transition name="fade">
-            <div v-if="showSuggestBox" class="suggest-box text-secondary">
-              <!-- Loader -->
-              <div v-if="loading" class="space-y-2">
-                <USkeleton v-for="i in 3" :key="i" class="h-20 w-full rounded-xl" />
-              </div>
-
-              <!-- Lawyers list -->
-              <div
-                v-else-if="lawyers?.length"
-                class="max-h-72 overflow-y-auto flex flex-col gap-2">
-                <NuxtLink
-                  v-for="lawyer in lawyers"
-                  :key="lawyer.id"
-                  :to="`/lawyer/${lawyer.id}`">
-                  <LawyerProfile
-                    class="border border-gray-200 p-3 rounded-xl"
-                    :information="{
-                      ...lawyer,
-                      lawyer_info: {
-                        name: lawyer.name,
-                        family: lawyer.family,
-                        profile_image: lawyer.profile_image,
-                      },
-                    }"
-                    :show="true"
-                    :show-experience="false"
-                    :ui="{
-                      base: 'items-start! w-full!',
-                      avatar: 'size-14! lg:size-16!',
-                      name: 'justify-between!',
-                    }">
-                    <template #badges>
-                      <UICBadge
-                        variant="yellow"
-                        icon-size="size-4! lg:size-5!"
-                        :value="Number(lawyer.average_rating).toFixed(1)"
-                        icon="ic:round-star" />
-                    </template>
-                  </LawyerProfile>
-                </NuxtLink>
-              </div>
-              <div
-                v-else
-                class="flex items-center gap-2 p-4 justify-center border border-gray-200 rounded-xl bg-gray-50">
-                <UIcon
-                  name="hugeicons:information-diamond"
-                  class="align-middle size-6!" />
-                <h1 class="font-semibold text-lg">وکیلی یافت نشد</h1>
-              </div>
-            </div>
-          </Transition>
+            </Transition>
+          </div>
+        </div>
+  
+        <div class="space-y-1.5">
+          <div class="latest-visit">
+            <UIcon name="ic:round-access-alarms" class="size-4!" />
+            7 دقیقه پیش
+          </div>
+          <p class="text-sm text-red-500">آخرین مشاوره دریافت شده در تخصص ثبت اسناد</p>
+        </div>
+        <div class="p-2 cursor-pointer flex custom-bounce" @click="navigateTo('#lawyers')">
+          <UIcon name="proicons:chevron-down" class="size-6! text-[#1B1893]" />
         </div>
       </div>
-
-      <div class="space-y-1.5">
-        <div class="latest-visit">
-          <UIcon name="ic:round-access-alarms" class="size-4!" />
-          7 دقیقه پیش
-        </div>
-        <p class="text-sm">آخرین مشاوره دریافت شده در تخصص ثبت اسناد</p>
-      </div>
-      <div class="p-2 cursor-pointer flex custom-bounce" @click="navigateTo('#lawyers')">
-        <UIcon name="proicons:chevron-down" class="size-6!" />
+      <div class="w-[40%] hidden lg:block">
+        <img src="/images/vector-lawyer.webp" alt="header-image" class="w-full h-full object-cover">
       </div>
     </div>
   </header>
@@ -226,28 +231,23 @@ onUnmounted(() => {
 @reference "tailwindcss";
 
 header {
-  @apply p-5 px-1 pt-9 rounded-b-[40px] text-center text-white;
-  background-image: radial-gradient(
-    circle 404px at 20.3% 15.9%,
-    rgba(0, 79, 255, 1) 0%,
-    rgb(0, 183, 255) 90%
-  );
+  @apply p-5 px-1 pt-9 rounded-b-[40px] bg-[#EEEEF8] text-center text-white;
 }
 
 .container {
-  @apply flex flex-col items-center gap-4 max-w-3xl;
+  @apply flex flex-row-reverse items-center justify-center lg:gap-6 lg:justify-between;
 }
 
 .header-title {
-  @apply text-[24px] md:text-[32px] font-bold pb-2;
+  @apply text-[24px] md:text-[32px] font-bold pb-2 text-[#242397];
 }
 
 .search-btn {
-  @apply aspect-square h-full bg-blue-400 rounded-full flex items-center justify-center cursor-pointer hover:opacity-70 transition duration-300;
+  @apply aspect-square h-full border border-[#00000047] bg-[#EEEEF8] rounded-full flex items-center justify-center cursor-pointer hover:opacity-70 transition duration-300;
 }
 
 .latest-visit {
-  @apply w-max mx-auto p-1 px-1.5 flex items-center gap-1.5 text-sm leading-3 rounded-full border border-dashed border-white;
+  @apply w-max mx-auto text-red-500 p-1 px-1.5 flex items-center gap-1.5 text-sm leading-3 rounded-full border border-dashed border-red-500;
 }
 
 @keyframes bounce-custom {
