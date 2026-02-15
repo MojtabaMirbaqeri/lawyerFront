5882ba893d420b5782bfe5a215f01834d99555a6
Aref pourhashemi <47208416+aref79@users.noreply.github.com>
2026-02-15 11:02:50 +0330

    fix show number

--- components/InfoLawyer/LawyerChooseVisit.vue
+++ components/InfoLawyer/LawyerChooseVisit.vue
@@ -20,19 +20,15 @@
           >
         </div> -->
         <div class="flex flex-col gap-2.5">
-          <!-- <UICSecondaryBtn
-            :disabled="!props.active"
-            @click="
-              navigateTo(
-                `/reserve/${$route.params.id}?visit_type=${
-                  items.find((item) => item.id === activeBtn)?.value
-                }`,
-              )
-            "
-            class="flex items-center justify-center rounded-[8px]!">
-            <span class="text-center text-base">ادامه فرآیند</span>
-          </UICSecondaryBtn> -->
-          <DirectCall :phone="phoneNumber" :lawyer-id="lawyerId" />
+          <DirectCall
+            v-if="phoneNumber"
+            :phone="phoneNumber"
+            :lawyer-id="lawyerId" />
+          <p
+            v-else
+            class="rounded-xl border border-slate-200/80 bg-slate-50/50 px-4 py-3 text-center text-sm text-slate-500">
+            وکیل نمایش شماره تماس را در سایت فعال نکرده است
+          </p>
         </div>
       </div>
     </div>
